979dfe201360cc63076bf3bea77ab364009b231e
Abdul Kaif <abdulkaifansari786@gmail.com>
2026-04-21 18:20:18 +0530

    increase rate limit

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ import helmet from "helmet";
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100,
+  max: 1000,
   message: "Too many requests from this IP, please try again later."
 });
 
